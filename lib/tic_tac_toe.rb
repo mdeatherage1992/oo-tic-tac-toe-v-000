88374744e461e6cb4aa48017cb5fc7e8d5d86172
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-12 14:09:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,6 @@
 class TicTacToe
 
-  def display_board
+  def board
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
     puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -38,18 +38,7 @@ def position_taken?(board, index)
 end
 
 
-WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
-
-def won?(board)
+def won?
   WIN_COMBINATIONS.detect do |mini|
   if  mini.all? { |space| board[space] == "X"}
     return mini
