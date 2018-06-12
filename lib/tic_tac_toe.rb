8ddc95aa36b8cb61800ee0aa7a76dd3509dbc239
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-12 16:29:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,15 +59,14 @@ end
 
 
 def turn
-  until winner
-    turn
-  end
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(index)
     move(index,current_player)
     display_board
+  else
+    turn
   end
 end
 
