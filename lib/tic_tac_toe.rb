13c8dcdf44a1f890679a75d8dc70d3680e5905a5
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-12 16:07:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -139,9 +139,6 @@ def input_to_index(user_input)
 end
 
 
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
 
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
