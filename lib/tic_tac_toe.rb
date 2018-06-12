e068ac511d6db5ed272985d039addb63337a167f
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-12 13:56:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,12 +1,12 @@
 class TicTacToe
 
-  #def display_board=(board)
-    #puts " #{board[0]} | #{board[1]} | #{board[2]} "
-    #puts "-----------"
-    #puts " #{board[3]} | #{board[4]} | #{board[5]} "
-    #puts "-----------"
-    #puts " #{board[6]} | #{board[7]} | #{board[8]} "
-  #end
+  def display_board
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
 
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
