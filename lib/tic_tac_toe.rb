592bbc9dfa3d4ed8457829122f9f6c6467d1b9d1
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-12 14:15:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,8 +15,7 @@ class TicTacToe
         puts " #{board[3]} | #{board[4]} | #{board[5]} "
         puts "-----------"
         puts " #{board[6]} | #{board[7]} | #{board[8]} "
-      end      
-  end
+      end    
 
   def current_player
    turn_count % 2 == 0 ? "X" : "O"
